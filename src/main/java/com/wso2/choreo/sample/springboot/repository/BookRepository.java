99ab449b8a8e4a91c40d1f703d2c4a19cc070814
Yomal Perera <yomal@192.168.1.2>
2023-08-28 23:04:26 +0530

    Fix schema2

--- src/main/java/com/wso2/choreo/sample/springboot/repository/BookRepository.java
+++ src/main/java/com/wso2/choreo/sample/springboot/repository/BookRepository.java
@@ -11,7 +11,7 @@ public class BookRepository {
     private Map<Integer, Book> map = new ConcurrentHashMap<>();
 
     public BookRepository() {
-        map.put(1, new Book(0, "Gulliver`s Travels", "Jonathan Swift", "READING"));
+        map.put(1, new Book(0, "Gulliver's Travels", "Jonathan Swift", "READING"));
         map.put(2, new Book(1, "Oliver Twist", "Charles Dickens", "READING"));
     }
 
